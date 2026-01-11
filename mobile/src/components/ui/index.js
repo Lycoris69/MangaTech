74f6218f7bdd97b6518e5bd8fa02751f744af653
lycoris69 <simeon.petrou@epitech.eu>
2026-01-11 21:53:13 +0100

    feat: fix ManhwaZ scraper, resolve Electron build issues, and implement Home Page infinite scroll

--- mobile/src/components/ui/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * UI Components - Cyber Neon Theme
- */
-
-export { default as NeonCard } from './NeonCard';
-export { default as GlowButton } from './GlowButton';
-export { default as HolographicText } from './HolographicText';
-export { default as GradientBackground } from './GradientBackground';
-export { default as MangaCard } from './MangaCard';
